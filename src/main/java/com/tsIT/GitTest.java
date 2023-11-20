9601162d713d519ac3d3f3838dcf919881b3ac5e
TianS <1342262404@qq.com>
2023-11-20 15:45:24 +0800

    second GitDemo commit

--- src/main/java/com/tsIT/GitTest.java
+++ src/main/java/com/tsIT/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hi git!");
         System.out.println("nice git3");
+        System.out.println("e git4");
     }
 }
