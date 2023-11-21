95190333ea90c7a7f1d323e63c47afedecb60483
TianS <1342262404@qq.com>
2023-11-21 14:51:56 +0800

    sdadwqqw

--- src/main/java/com/tsIT/GitTest.java
+++ src/main/java/com/tsIT/GitTest.java
@@ -6,11 +6,11 @@ public class GitTest {
         System.out.println("hi git!");
         System.out.println("nice git3");
         System.out.println("e git4");
-        System.out.println("merge git5");
-        System.out.println("push git5");
+
         System.out.println("pull git5");
         System.out.println("pull2 git5");
         System.out.println("dasdas");
-        System.out.println("dasdasdas");
+
+
     }
 }
