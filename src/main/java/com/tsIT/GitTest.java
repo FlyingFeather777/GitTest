b52777c5239d7d2435743e6f20809e837797084b
TianS <1342262404@qq.com>
2023-11-21 14:56:25 +0800

    sdadwqqw

--- src/main/java/com/tsIT/GitTest.java
+++ src/main/java/com/tsIT/GitTest.java
@@ -3,11 +3,7 @@ package com.tsIT;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hi git!");
-        System.out.println("nice git3");
-        System.out.println("e git4");
-        System.out.println("merge git5");
-        System.out.println("push git5");
+
         System.out.println("pull git5");
         System.out.println("pull2 git5");
         System.out.println("dasdas");
